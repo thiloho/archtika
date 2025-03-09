4afd58c24d418c6ddd9c6f6a7e2cd51f245d3a99
thiloho <123883702+thiloho@users.noreply.github.com>
2025-03-09 16:37:01 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           web = pkgs.mkShell {
             packages = with pkgs; [ nodejs ];
             shellHook = ''
-              export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+              export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright.browsers}
               export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
             '';
           };
